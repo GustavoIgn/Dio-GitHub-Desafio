156c041de1167d3038ae0a7c9aa2c2382aa3bdd0
GustavoIgn <gustavoignacio280@gmail.com>
2023-02-28 02:20:55 -0300

    Mais operadores.

--- tipos-operadores/src/TiposOperadores.java
+++ tipos-operadores/src/TiposOperadores.java
@@ -15,7 +15,20 @@ public class TiposOperadores {
 
 
     System.out.println("\nMe chamo " + nome + ", tenho " + idade + " anos.\n");
-    System.out.print("O resultado de cada operação é: " + soma + ", " + subtracao + ", " + multiplicacao + ", " + divisao + ", " + resultado + ".");
+    System.out.println("O resultado de cada operação é: " + soma + ", " + subtracao + ", " + multiplicacao + ", " + divisao + ", " + resultado + ".\n");
+
+    /*Operador Unário, aplicados com outro operador aritmético.
+    (+) Operador unário de valor positivo – números são positivos sem este explicitamente;
+    (-) Operador unário de valor negativo – nega um número ou expressão aritmética;
+    (++) Operador unário de incremento de valor – incrementa o valor em 1 unidade;
+    (--) Operador unário de decremento de valor – decrementa o valor em 1 unidade;
+    (!) Operador unário lógico de negação – nega o valor de uma expressão booleana.*/
+        int número = 6;
+    System.out.println(- número + ". \n");
+        número ++;
+    System.out.print(número + ". \n");    
+
+
 
     }
 }
